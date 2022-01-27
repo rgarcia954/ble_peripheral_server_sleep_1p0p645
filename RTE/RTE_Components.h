126f5b9bfdfc2d344d7bbd5c558be2f585928777
Rudy Garcia <rgarcia@dexcom.com>
2022-01-27 13:17:30 -0500

    Add UART instead of RTT debug.

--- RTE/RTE_Components.h
+++ RTE/RTE_Components.h
@@ -2,8 +2,8 @@
  * Auto generated Run-Time-Environment Component Configuration File
  *      *** Do not modify ! ***
  *
- * Project: ble_peripheral_server_sleep
- * RTE configuration: ble_peripheral_server_sleep.rteconfig
+ * Project: ble_peripheral_server_sleep_1p0p645
+ * RTE configuration: ble_peripheral_server_sleep_1p0p645.rteconfig
 */
 #ifndef RTE_COMPONENTS_H
 #define RTE_COMPONENTS_H
@@ -17,8 +17,8 @@
 #define RTE_ADDED_BLE_ABSTRACTION
 /* ONSemiconductor::Device.Bluetooth Profiles.BASS.release */
 #define RTE_ADDED_PRF_BASS
-/* ONSemiconductor::Device.Libraries.SwmTrace.swmTrace_RTT_NB_Source */
+/* ONSemiconductor::Device.Libraries.SwmTrace.swmTrace_UART_NB_Source */
 #define SWMTRACE_ENABLEMENT SWMTRACE_ENABLED
-       #define SWM_TRACE_TYPE SWM_TRACE_SEGGER_RTT_NON_BLOCKING
+       #define SWM_TRACE_TYPE SWM_TRACE_UART_NON_BLOCKING
 
 #endif /* RTE_COMPONENTS_H */
